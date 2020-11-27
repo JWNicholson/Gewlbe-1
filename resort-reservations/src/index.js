889f26a7478dda83f35dd936b201f6b03ce53d1c
JW Nicholson <john.nicholson812@gmail.com>
2020-11-27 12:28:46 -0500

    adding context for room data

--- resort-reservations/src/index.js
+++ resort-reservations/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import { RoomProvider } from './context';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  
+
   <React.StrictMode>
-    <Router>
-    <App />
-    </Router>
+    <RoomProvider >
+      <Router>
+        <App />
+      </Router>
+    </RoomProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );

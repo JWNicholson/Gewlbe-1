54a08ae1becdce3d444f59e2901b3beab5d7cbdc
JW Nicholson <john.nicholson812@gmail.com>
2020-12-01 11:50:47 -0500

    added content to game boxes.no logic yet

--- m-game-1/src/components/TicTacToe.js
+++ m-game-1/src/components/TicTacToe.js
@@ -6,10 +6,154 @@ export default function TicTacToe() {
             <section className="sectionHeader">
                 <h2>Tic-Tac-Toe</h2>
             </section>
-            <div className="game-tictactoe">
 
+            <div className="game-wrapper">
+                <div class="game-box box-1">
+                    <div className="game-box-content">
+                        <h2>X</h2>
 
-                
+                        <div className="game-box-label-ctnr">
+                            <label>
+                            <input type="radio" value="X" name="ttt-choice" />
+                            X
+                            </label>
+                            <label>
+                            <input type="radio" value="O" name="ttt-choice" />
+                            O
+                            </label>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="game-box box-2">
+                <div className="game-box-content">
+                        <h2>X</h2>
+
+                        <div className="game-box-label-ctnr">
+                            <label>
+                            <input type="radio" value="X" name="ttt-choice" />
+                            X
+                            </label>
+                            <label>
+                            <input type="radio" value="O" name="ttt-choice" />
+                            O
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="game-box box-3">
+                <div className="game-box-content">
+                        <h2>X</h2>
+
+                        <div className="game-box-label-ctnr">
+                            <label>
+                            <input type="radio" value="X" name="ttt-choice" />
+                            X
+                            </label>
+                            <label>
+                            <input type="radio" value="O" name="ttt-choice" />
+                            O
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="game-box box-4">
+                <div className="game-box-content">
+                        <h2>X</h2>
+
+                        <div className="game-box-label-ctnr">
+                            <label>
+                            <input type="radio" value="X" name="ttt-choice" />
+                            X
+                            </label>
+                            <label>
+                            <input type="radio" value="O" name="ttt-choice" />
+                            O
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="game-box box-5">
+                <div className="game-box-content">
+                        <h2>X</h2>
+
+                        <div className="game-box-label-ctnr">
+                            <label>
+                            <input type="radio" value="X" name="ttt-choice" />
+                            X
+                            </label>
+                            <label>
+                            <input type="radio" value="O" name="ttt-choice" />
+                            O
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="game-box box-6">
+                <div className="game-box-content">
+                        <h2>X</h2>
+
+                        <div className="game-box-label-ctnr">
+                            <label>
+                            <input type="radio" value="X" name="ttt-choice" />
+                            X
+                            </label>
+                            <label>
+                            <input type="radio" value="O" name="ttt-choice" />
+                            O
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="game-box box-7">
+                <div className="game-box-content">
+                        <h2>X</h2>
+
+                        <div className="game-box-label-ctnr">
+                            <label>
+                            <input type="radio" value="X" name="ttt-choice" />
+                            X
+                            </label>
+                            <label>
+                            <input type="radio" value="O" name="ttt-choice" />
+                            O
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="game-box box-8">
+                <div className="game-box-content">
+                        <h2>X</h2>
+
+                        <div className="game-box-label-ctnr">
+                            <label>
+                            <input type="radio" value="X" name="ttt-choice" />
+                            X
+                            </label>
+                            <label>
+                            <input type="radio" value="O" name="ttt-choice" />
+                            O
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="game-box box-9">
+                <div className="game-box-content">
+                        <h2>X</h2>
+
+                        <div className="game-box-label-ctnr">
+                            <label>
+                            <input type="radio" value="X" name="ttt-choice" />
+                            X
+                            </label>
+                            <label>
+                            <input type="radio" value="O" name="ttt-choice" />
+                            O
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     )

67be44b03880c6f752b6e9d6edebbef16afaeb35
JW Nicholson <john.nicholson812@gmail.com>
2020-12-02 14:24:14 -0500

    Add files via upload

---
+++ Code-practice/one.js
@@ -0,0 +1,20 @@
+//////////one 
+//Reverse only words, leaving them and any punction in same place
+////
+// assign sentence to a variable
+
+let phrase = "He walked into the basement, and it was dark."
+
+// split into array
+let stepOne = phrase.split(" ")
+
+//
+let x = stepTwo
+let y = x.reverse()
+
+console.log(stepOne)
+console.log(y)
+
+document.getElementById('solution-1').innerHTML = phrase ;
+
+/////////end one
